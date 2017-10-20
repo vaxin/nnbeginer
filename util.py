cb51d2f1beb3c70fa74f61d2fdbd0dcd1797f910
dengfanxin <dengfanxin@gmail.com>
2017-10-20 13:37:02 +0800

    finish lenet

--- util.py
+++ util.py
@@ -2,13 +2,16 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 from torchvision import transforms, datasets
+import torch.optim as optim
+import numpy as np
 
+# load mnist
 def minst_loader():
     # load mnist
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ])  
 
     train_set = datasets.MNIST('./data', train = True, download = True, transform = transform)
     test_set = datasets.MNIST('./data', train = False, download = True, transform = transform)
@@ -17,21 +20,21 @@ def minst_loader():
     test_loader = torch.utils.data.DataLoader(train_set, shuffle = False)
     return (train_loader, test_loader)
 
-import torch.optim as optim
-from torch.autograd import Variable
-import numpy as np
+
 def train(data_loader, n_epoch, net, criterion):
     optimizer = optim.SGD(net.parameters(), lr = 0.01, momentum = 0.5)
 
     for epoch in range(n_epoch):
         loss_arr = []
         accuracy_arr = []
+        print("------ epoch: %d Start--------" % (epoch))   
         for i, data in enumerate(data_loader, 0): # 0的意思是无论重跑几次，都从0开始迭代，避免多次试验时不稳定的问题
             inputs, labels = data
             inputs, labels = Variable(inputs), Variable(labels)
             
             optimizer.zero_grad() # 等同于net.zero_grad()
             outputs = net(inputs)
+            
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -40,7 +43,12 @@ def train(data_loader, n_epoch, net, criterion):
             acc = (predicted == labels).long().sum()
             total = labels.size(0)
 
-            loss_arr.append(loss.data[0])
-            accuracy_arr.append(acc.data[0] * 100 / total)
+            loss = loss.data[0]
+            acc = acc.data[0] * 100 / total
+            
+            loss_arr.append(loss)
+            accuracy_arr.append(acc)
+            if i % 100 == 0:
+                print('iter: %d, Loss: %.2f, Accuracy: %.2f %%' % (i, loss, acc))
           
-        print("epoch: %d, Loss: %.2f, Accuracy: %.2f %%" % (epoch, np.sum(loss_arr) / len(loss_arr), np.sum(accuracy_arr) / len(accuracy_arr)))   
+        print("------ epoch: %d, Loss: %.2f, Accuracy: %.2f %%--------" % (epoch, np.sum(loss_arr) / len(loss_arr), np.sum(accuracy_arr) / len(accuracy_arr)))   
